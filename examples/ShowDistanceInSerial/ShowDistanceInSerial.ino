e7d877a7796f628d7f8919b1c881fa80b11a3af3
Quentin Comte-Gaz <quentin@comte-gaz.com>
2021-12-27 21:18:19 +0100

    Minor update for code consistency

--- examples/ShowDistanceInSerial/ShowDistanceInSerial.ino
+++ examples/ShowDistanceInSerial/ShowDistanceInSerial.ino
@@ -2,9 +2,9 @@
  * \brief Show distance between Ultrasonic proximity sensor and any element every second
  *
  * \author Quentin Comte-Gaz <quentin@comte-gaz.com>
- * \date 3 July 2016
+ * \date 27 December 2021
  * \license MIT License (contact me if too restrictive)
- * \copyright Copyright (c) 2016 Quentin Comte-Gaz
+ * \copyright Copyright (c) 2021 Quentin Comte-Gaz
  * \version 1.0
  */
 
@@ -27,11 +27,14 @@ void loop()
 {
   unsigned long distance_in_cm = proximity_sensor.getDistanceInCm();
 
-  if (distance_in_cm <= 0) {
+  if (distance_in_cm <= 0)
+  {
     Serial.print("Sensor not connected or distance higher than ");
     Serial.print(proximity_sensor.getMaximumDistanceToCheckInCm());
     Serial.print("cm\n");
-  } else {
+  }
+  else
+  {
     Serial.print("Distance: ");
     Serial.print(distance_in_cm);
     Serial.print("cm\n");
